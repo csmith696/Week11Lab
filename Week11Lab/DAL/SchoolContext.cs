d72d87385b8e6f245909cc62dc4046d3163dce08
csmith696 <95193857+csmith696@users.noreply.github.com>
2022-12-06 15:50:51 -0500

    Add project files.

---
+++ Week11Lab/DAL/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Week11Lab.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Week11Lab.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext") { }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Week11Lab/DAL/SchoolInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Week11Lab.Models;
+
+namespace Week11Lab.DAL
+{
+    public class SchoolInitializer : DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        protected override void Seed(SchoolContext context)
+        {
+            var students = new List<Student>
+ {
+ new
+Student{FirstName="Carson",MiddleName="Joe",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+ new
+Student{FirstName="Meredith",MiddleName="John",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+ new
+Student{FirstName="Arturo",MiddleName="Arto",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+ new
+Student{FirstName="Gytis",MiddleName="Godo",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+ new
+Student{FirstName="Yan",MiddleName="Mu",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+ new
+Student{FirstName="Peggy",MiddleName="Sue",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+ new
+Student{FirstName="Laura",MiddleName="Lynn",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+ new
+Student{FirstName="Nino",MiddleName="Jack",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+ };
+            students.ForEach(s => context.Students.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
